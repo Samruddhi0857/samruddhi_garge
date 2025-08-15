829bc4675e5404a66da5eb8c36362a20c7cd0055
Samruddhi0857 <166351795+Samruddhi0857@users.noreply.github.com>
2025-08-15 22:30:59 +0530

    Update dsa_day1.cpp

--- dsa_day1.cpp
+++ dsa_day1.cpp
@@ -35,6 +35,11 @@ int main()
      int n;
     cout<<"enter number of elements : ";
     cin>>n;
+    if (n == 0) {
+    cout << "Sorted array: []\n";
+    return 0;
+}
+
     vector<int>  arr(n);
    
     for(int i=0;i<n;i++)
