ede9369c80cc8e1b1bcc01edaf567e65f22cf133
Samruddhi0857 <166351795+Samruddhi0857@users.noreply.github.com>
2025-09-06 12:49:03 +0530

    Update dsa_day23.cpp

--- dsa_day23.cpp
+++ dsa_day23.cpp
@@ -1,25 +1,73 @@
 #include <iostream>
 #include <vector>
-#include <deque>
 using namespace std;
+
+
+class Deque
+{
+    vector<int> dq;      
+    int frontIdx, backIdx, capacity, count;
+
+public:
+    Deque(int n) {
+        dq.resize(n);
+        frontIdx = 0;
+        backIdx = -1;
+        capacity = n;
+        count = 0;
+    }
+
+    void push_back(int x) {
+        if (count == capacity) return; 
+        backIdx = (backIdx + 1) % capacity;
+        dq[backIdx] = x;
+        count++;
+    }
+
+    void pop_back() {
+        if (empty()) return;
+        backIdx = (backIdx - 1 + capacity) % capacity;
+        count--;
+    }
+
+    void pop_front() {
+        if (empty()) return;
+        frontIdx = (frontIdx + 1) % capacity;
+        count--;
+    }
+
+    int back() {
+        return empty() ? -1 : dq[backIdx];
+    }
+
+    int front() {
+        return empty() ? -1 : dq[frontIdx];
+    }
+
+    bool empty() {
+        return count == 0;
+    }
+};
+
 
 vector<int> slidingWindowMax(vector<int>& arr, int k) {
     int n = arr.size();
-    deque<int> dq;  
-       vector<int> result;
+    Deque dq(n); 
+    vector<int> result;
 
     for (int i = 0; i < n; i++) {
        
         if (!dq.empty() && dq.front() <= i - k)
-               dq.pop_front();
+            dq.pop_front();
 
-        
+       
         while (!dq.empty() && arr[dq.back()] < arr[i])
             dq.pop_back();
 
-   
+        
         dq.push_back(i);
 
+       
         if (i >= k - 1)
             result.push_back(arr[dq.front()]);
     }
@@ -29,19 +77,19 @@ vector<int> slidingWindowMax(vector<int>& arr, int k) {
 
 int main() {
     int n, k;
-      cout << "Enter number of elements: ";
+    cout << "Enter number of elements: ";
     cin >> n;
 
     vector<int> arr(n);
-        cout << "Enter array: ";
+    cout << "Enter array elements: ";
     for (int i = 0; i < n; i++) cin >> arr[i];
 
-    cout << "Enter k: ";
+    cout << "Enter window size k: ";
     cin >> k;
 
     vector<int> ans = slidingWindowMax(arr, k);
 
-    cout << "Sliding Window Max eles: ";
+    cout << "Sliding Window Maximums: ";
     for (int x : ans) cout << x << " ";
     cout << endl;
 
